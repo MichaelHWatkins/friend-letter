9d4975933ddd32a72b9d6fa95c3574d63788a66d
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-12-13 10:57:19 -0800

    it will add the rest fo the program
    Co-authored-by: Jeff Terrell <jeffaterrell@gmail.com>

---
+++ FriendLetter.Solution/FriendLetter/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using FriendLetter.Models;
+
+
+namespace FriendLetter.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [Route("/hello")]
+    public string Hello() { return "Hello friend!"; }
+
+    [Route("/goodbye")]
+    public string Goodbye() { return "Goodbye friend."; }
+
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
+
+    [Route("/postcard")]
+    public ActionResult Postcard(string recipient, string sender)
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.Recipient = recipient;
+      myLetterVariable.Sender = sender;
+      return View(myLetterVariable);
+    }
+
+    [Route("/")]
+    public ActionResult Letter()
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.Recipient = "Lina";
+      myLetterVariable.Sender = "Jasmine";
+      myLetterVariable.Location = "Iceland";
+      return View(myLetterVariable);
+    }
+  }
+}
